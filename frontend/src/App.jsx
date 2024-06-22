b011aa19f04eced57b8f4af6e2ef9c46ad9e84d6
lamiabelhadj <lamia.belhadjsghaier@insat.ucar.tn>
2024-06-22 12:33:24 +0100

    deleted unecessary files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import TaskList from './pages/TaskList';
-import TaskDetails from './pages/TaskDetails';
 import CreateTask from './pages/CreateTask';
 import EditTask from './pages/EditTask';
 
@@ -9,7 +8,6 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<TaskList />} />
-      <Route path="/tasks/:id" element={<TaskDetails />} />
       <Route path="/create-task" element={<CreateTask />} />
       <Route path="/edit-task/:id" element={<EditTask />} />
     </Routes>
