9f8a0ae9e161a31c30658443c85502f49d4c1003
lamiabelhadj <lamia.belhadjsghaier@insat.ucar.tn>
2024-06-22 12:47:42 +0100

    created user.js

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+
+userSchema.pre('save', async function(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  const hash = await bcrypt.hash(user.password, 10);
+  user.password = hash;
+  next();
+});
+
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
