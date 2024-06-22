d5b0a98f75bf947f218d5b9625193d18db4170e5
lamiabelhadj <lamia.belhadjsghaier@insat.ucar.tn>
2024-06-22 10:33:34 +0100

    backend configuration

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {PORT} from "./config.js";
+import mongoose from 'mongoose';
+import {mongoDBURL} from "./.env";
+
+//require('dotenv').config();
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request,response) => {
+    console.log(request)
+    return response.status(234).send('hello');
+});
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`app is listening to port : ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
